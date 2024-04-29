4f5f723bdbb34054e2fe7c8f59beab7132104d8a
PC <PC@DESKTOP-51E0I65>
2024-04-29 21:22:21 +0300

    Entites created.

---
+++ Apartment.Data/Entities/ApartmentDuesPayments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartment.Data.Entities
+{
+    public class ApartmentDuesPayments
+    {
+        [Required]
+        public int paymentId { get; set; }
+        [Required]
+        public decimal duesAmount { get; set; }
+        [Required]
+        public int doorId { get; set; }
+        [Required]
+        public DateTime paymentDate { get; set; }
+    }
+}
---
+++ Apartment.Data/Entities/ResidentRequests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartment.Data.Entities
+{
+    public class ResidentRequests
+    {
+        [Required]
+        public int requestId { get; set; }
+        [Required]
+        public int residentId { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
